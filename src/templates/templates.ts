defd029a5051f9ce7d71ce986b462c183d591018
Diego Castro <diego.castro@evolvingweb.com>
2023-12-08 12:41:40 -0500

    Adding templates files for SDCs

---
+++ src/templates/templates.ts
@@ -0,0 +1,11 @@
+const Templates = {
+  SDC: [
+    'sdc/component.component.yml.ejs',
+    'sdc/component.css.ejs',
+    'sdc/component.js.ejs',
+    'sdc/component.twig.ejs',
+    'sdc/README.md.ejs',
+  ],
+};
+
+export default Templates;
