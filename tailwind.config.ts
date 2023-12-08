97e35f9ecbe6f597696d07cb77911d4fd7b6faa2
Diego Castro <diego.castro@evolvingweb.com>
2023-12-08 12:18:20 -0500

    Adding base colors to tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      // https://color.adobe.com/trends/Travel
+      colors: {
+        'pacific-blue': '#04ADBF',
+        'robin-egg-blue': '#04BFBF',
+        'pine-green': '#027373',
+        'olive-drab': '#7EA629',
+        'parchment': '#F2E3D5',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
