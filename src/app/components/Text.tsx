fa04fb618723158466ee09af0faab5e16bc17f25
Diego Castro <diego.castro@evolvingweb.com>
2023-12-16 18:30:55 -0500

    Dark theme improvement.

--- src/app/components/Text.tsx
+++ src/app/components/Text.tsx
@@ -17,12 +17,12 @@ export const Text = ({
         type="text" id={id}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-olive-drab focus:outline-none focus:ring-0 focus:border-olive-drab peer"
+        className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:bg-gray-700 dark:focus:border-olive-drab focus:outline-none focus:ring-0 focus:border-olive-drab peer"
         placeholder=" "
       />
       <label
         htmlFor={id}
-        className="absolute text-sm rounded-md text-pine-green dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white/50 backdrop-blur-md dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-olive-drab peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+        className="absolute text-sm rounded-md text-pine-green dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white/50 backdrop-blur-md dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-olive-drab peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
         {label}
       </label>
     </div>
